a44049deb2b6fa7c776ed6e3dd8caeb78900e762
Elena Russkykh <lenarusskih@gmail.com>
2025-06-03 15:35:25 +0300

    add files lab4

---
+++ Labs/lab4/script.js
@@ -0,0 +1,7 @@
+// Завдання 1
+document.getElementById("showButton").addEventListener("click", function() {
+    const inputValue = document.getElementById("inputField").value;
+    console.log("Введене значення:", inputValue);
+});
+
+// Завдання 3
